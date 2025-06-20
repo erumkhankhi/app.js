ccfd0a7e6b1cd3c89cee298e88d32a211a63adb6
Erum Khan <erumkhankhi@gmail.com>
2025-06-21 01:05:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// alert("Error! please enter your valid password");
+// alert("Welcom \n Happy coding");
+// alert("Welcome to is Land...");
+// alert("Happy coding!");
+// alert("Hello... Ican run js through my web")
+// function showMessage() {
+//   alert("Hello! Welcome to the website!");
+// }
+// PART 2ND
+
+// Q NO ONE
+
+// alert("This is an alert message!");
+// let studentNames = [];
+// studentNames.push("Ali");
+// studentNames.push("Sara");
+// console.log(studentNames); // Output: ["Ali", "Sara"]
+
+// Q NO TWO
+
+// let myName = "ERUM KHAN";
+
+// Q NO THREE
+// IN HTML
+
+// Q NO FOUR
+
+// let studentName = "Ali Raza";
+//     let age = 17;
+//     let studentClass = "10th Grade";
+//     let city = "Lahore";
+
+//     // Show the data in alert boxes
+//     alert("Name: " + studentName);
+//     alert("Age: " + age);
+//     alert("Class: " + studentClass);
+//     alert("City: " + city);
+
+// Q FIVE
+
+// let message = "Welcome to JavaScript!\nThis is an alert box.\nHave a great day!";
